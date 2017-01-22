9285d0bea47ef4d7b235d2f7ec4553af8600db11
BoazB <berman.boaz@gmail.com>
2017-01-22 16:49:30 +0200

    Updated the ex

--- ex5.py
+++ ex5.py
@@ -2,65 +2,75 @@ import sys
 from PIL import Image
 import numpy as np
 
-K_MEANS_NUMBER_ITERATIONS = 10
+ITERATIONS = 10
+
 
 def parse_data(initial_centroids_path, image_file_path):
-    im = Image.open(image_file_path)
-    x = np.array(im)
+    x = np.array(Image.open(image_file_path))
     centroids = np.loadtxt(initial_centroids_path)
 
-    return centroids,x
+    return centroids, x
 
-def calc_new_centroids(centroids, image):
-    clusters_pixels = {}
-    for i in xrange(K_MEANS_NUMBER_ITERATIONS):
-        # classify to clusters
-        for row in image:
-            for pixel in row:
-                cluster_id = np.argmin([np.linalg.norm(pixel - c) for c in centroids])
-                if cluster_id not in clusters_pixels:
-                    clusters_pixels[cluster_id] = []
-                clusters_pixels[cluster_id].append(pixel)
-        # update centroids
-        for idx,c in enumerate(centroids):
-            if len(clusters_pixels[idx]) != 0:
-                centroids[idx] = np.sum([arr for arr in clusters_pixels[idx]], axis=0) / len(clusters_pixels[idx])
-        clusters_pixels = {}
 
-    for c in centroids:
-        print "%s" % c
+def calculate_centroids(centroids, image):
+    for _ in xrange(ITERATIONS):
+        clustered_pixels = classify(centroids, image)
+        recalculate_centroids(centroids, clustered_pixels)
+
+    for row in centroids:
+        for cell in row:
+            print "%s" % int(cell)
 
     return centroids
+
+
+def recalculate_centroids(centroids, clusters_pixels):
+    for i in xrange(len(centroids)):
+        if len(clusters_pixels[i]) != 0:
+            centroids[i] = np.sum(clusters_pixels[i], axis=0) / len(clusters_pixels[i])
+
+
+def classify(centroids, image):
+    clusters = {}
+    for row in image:
+        for pixel in row:
+            cluster_id = np.argmin([np.linalg.norm(pixel - c) for c in centroids])
+            if cluster_id not in clusters:
+                clusters[cluster_id] = []
+            clusters[cluster_id].append(pixel)
+    return clusters
+
 
 def find_nearest_centroid(pixel, centroids):
     return centroids[np.argmin([np.linalg.norm(pixel - c) for c in centroids])]
 
 
-def calc_new_image(image, centroids):
-    new_image = image
-    for image_idx,row in enumerate(image):
-        new_row = row
-        for row_idx,pixel in enumerate(row):
-            new_row[row_idx] = find_nearest_centroid(pixel,centroids)
-        new_image[image_idx] = new_row
+def recalc_image(image, centroids):
+    for row in image:
+        for i, pixel in enumerate(row):
+            row[i] = find_nearest_centroid(pixel, centroids)
 
-    return new_image
+    return image
 
 
-def save_new_image(new_image,image_file_name):
-    x = Image.fromarray(new_image)
-    x.save(image_file_name+'_comp.tif')
+def save_new_image(new_image, image_file_name):
+    Image.fromarray(new_image).save(image_file_name + '_comp.tif')
 
 
 def extract_file_name(file_path):
     if "\\" in file_path:
-        return file_path.rsplit("\\",1)[1].split(".")[0]
-    return file_path.split(".")[0]
+        file_path = file_path.rsplit("\\", 1)[1]
+    return file_path.rsplit(".", 1)[0]
+
+
+def main(args):
+    initial_centroids_path = args[0]
+    image_file_path = args[1]
+    centroids, image = parse_data(initial_centroids_path, image_file_path)
+    new_centroids = calculate_centroids(centroids, image)
+    new_image = recalc_image(image, new_centroids)
+    save_new_image(new_image, extract_file_name(image_file_path))
+
 
 if __name__ == "__main__":
-    initial_centroids_path = sys.argv[1]
-    image_file_path = sys.argv[2]
-    centroids,image = parse_data(initial_centroids_path, image_file_path)
-    new_centroids = calc_new_centroids(centroids, image)
-    new_image = calc_new_image(image, new_centroids)
-    save_new_image(new_image, extract_file_name(image_file_path))
+    main(sys.argv[1:])
